b7154dba8c94c79afe56339ab58fdf41c94347ec
Jay Campbell <jay.campbell-luo@consilium.co.nz>
2026-03-18 16:46:36 +1300

    add

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import datetime
 import polars as pl
 import numpy as np
+import matplotlib.pyplot as plt
 from dateutil.relativedelta import relativedelta
 
 # takes in a pl.Date in the format <month><year>
@@ -24,8 +25,7 @@ def interp_price(x: list[float], y: list[float], target: float):
         y0, y1 = y[i], y[i + 1]
         
         if x0 <= target <= x1:
-            return y0
-        return y0 + (target - x0) * (y1 - y0) / (x1 - x0)
+            return y0 + (target - x0) * (y1 - y0) / (x1 - x0)
     
     return None
 
@@ -40,7 +40,7 @@ def interp_targets(group: pl.DataFrame):
         row[f"t_{int(target)}"] = interp_price(dte, prices, target)
         
     # we have to manually define the scheme in case there are nulls in our output,
-    # which tends to happen around the start of the data where we don't have full data
+    # which tends to happen around the start of the data
     return pl.DataFrame([row], schema={
         "Trade Date": group.schema["Trade Date"],
         "t_7": pl.Float64,
@@ -60,9 +60,90 @@ def add_cumret(
         base = 1
     
     return df.with_columns(
-        (pl.col(return_col) * pl.col(flag_col).shift(1)).alias(f"return earned: {flag_col} (shifted)")
+        (pl.col(return_col) * pl.col(flag_col).shift(-1)).alias(f"return earned: {flag_col} (shifted)")
     ).with_columns(
         ((pl.col(f"return earned: {flag_col} (shifted)")/base + 1).cum_prod().alias(f"cumret: {flag_col}"))
     )
     
+def build_ratio_table(df: pl.DataFrame, front_pd: str, back_pd: str) -> pl.DataFrame:
+    df = df.select([
+        pl.col("Trade Date"),
+        pl.col("BATS:VIXY - Return"),
+        pl.col(f"t_{front_pd}"),
+        pl.col(f"t_{back_pd}")
+    ])
+    df = df.with_columns([
+        (pl.col(f"t_{front_pd}") / pl.col(f"t_{back_pd}")).alias(f"{front_pd}/{back_pd} Ratio"),
+        ((pl.col(f"t_{front_pd}") / pl.col(f"t_{back_pd}")) > 1).alias(f"{front_pd}/{back_pd} inversion"),
+        pl.col("BATS:VIXY - Return").shift(-1).alias("BATS:VIXY - Return (Shifted)")
+    ])
     
+    # doing some cleanup here:
+    #   dropping nulls from .shift(1)
+    #   have some data issues on the tail end because our interpolation func uses the last value if it doesn't
+    #   have a back data point to interpolate from. this leads to our ratio becoming exactly 1.0 for the last few obs
+    df = df.drop_nulls()
+    df = df.filter((pl.col(f"{front_pd}/{back_pd} Ratio") != 1.0))
+    
+    return df
+    
+def build_strategy_table(df: pl.DataFrame, front_pd: str, back_pd: str) -> pl.DataFrame:
+    df = df.select([
+        pl.col("Trade Date"),
+        pl.col("BATS:VIXY - Return"),
+        pl.col(f"t_{front_pd}"),
+        pl.col(f"t_{back_pd}")
+    ])
+    df = df.with_columns([
+        (pl.col(f"t_{front_pd}") / pl.col(f"t_{back_pd}")).alias(f"{front_pd}/{back_pd} Ratio"),
+        ((pl.col(f"t_{front_pd}") / pl.col(f"t_{back_pd}")) > 1).alias(f"{front_pd}/{back_pd} inversion"),
+        (((pl.col(f"t_{front_pd}") / pl.col(f"t_{back_pd}")) > 1).shift(1) * pl.col("BATS:VIXY - Return")).alias("Return Earned")
+    ])
+    
+    df = df.drop_nulls()
+    df = df.filter((pl.col(f"{front_pd}/{back_pd} Ratio") != 1.0))
+    
+    return df 
+    
+def bucket_on_ratio(df: pl.DataFrame, bucket_ratio: str, fwd: bool = True, n_buckets: int = 4) -> pl.DataFrame:
+    bucket = df.with_columns(pl.col(f"{bucket_ratio} Ratio").qcut(n_buckets).alias(f"Ratio Bucket {bucket_ratio}"))
+    out = (
+        bucket.group_by(f"Ratio Bucket {bucket_ratio}")
+            .agg([
+                pl.len().alias("n"),
+                pl.col(f"{bucket_ratio} Ratio").mean().alias("Average Ratio"),
+                pl.col(f"BATS:VIXY - Return{' (Shifted)' if fwd else ''}").mean().alias("Mean Return"),
+                pl.col(f"BATS:VIXY - Return{' (Shifted)' if fwd else ''}").median().alias("Median Return"),
+                pl.col(f"BATS:VIXY - Return{' (Shifted)' if fwd else ''}").std().alias("Std Dev"),                
+            ]).sort(by=f"Ratio Bucket {bucket_ratio}")
+    )
+    
+    return out
+
+def bucket_on_ratio_inverted(df: pl.DataFrame, bucket_ratio: str, fwd: bool = True, n_buckets: int = 4) -> pl.DataFrame:
+    inverted = df.filter(pl.col(f"{bucket_ratio} Ratio") > 1)
+    bucket = inverted.with_columns(pl.col(f"{bucket_ratio} Ratio").qcut(n_buckets).alias(f"Ratio Bucket {bucket_ratio}"))
+    out = (
+        bucket.group_by(f"Ratio Bucket {bucket_ratio}")
+            .agg([
+                pl.len().alias("n"),
+                pl.col(f"{bucket_ratio} Ratio").mean().alias("Average Ratio"),
+                pl.col(f"BATS:VIXY - Return{' (Shifted)' if fwd else ''}").mean().alias("Mean Return"),
+                pl.col(f"BATS:VIXY - Return{' (Shifted)' if fwd else ''}").median().alias("Median Return"),
+                pl.col(f"BATS:VIXY - Return{' (Shifted)' if fwd else ''}").std().alias("Std Dev"),
+                (pl.col(f"BATS:VIXY - Return{' (Shifted)' if fwd else ''}") > 0).mean().alias("Hit Rate")
+            ]).sort(by=f"Ratio Bucket {bucket_ratio}")
+    )
+    
+    return out
+
+def draw_wealth(df: pl.DataFrame, ratio: str, base: int | None = 100) -> pl.DataFrame:
+    df = df.with_columns(
+        ((pl.col("Return Earned") / base + 1).cum_prod()).alias(f"{ratio} Cumulative Return")
+    )
+    
+    fig, ax = plt.subplots()
+    plt.plot(df["Trade Date"], df[f"{ratio} Cumulative Return"])
+    plt.show()
+    
+    return df
